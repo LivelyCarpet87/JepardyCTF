e7465cfb6f8737420df4cc1f837ff1f7c7c4b751
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-12-07 00:29:29 +0800

    Blacklist Directories To Require Logins

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ from serverBackend import adminFunc, info, scorebot, data
 app = flask.Flask(__name__)
 port = int(os.getenv('PORT', 8080))
 
-app.register_blueprint(Crypto.crypto.crypto, url_prefix='/Crypto')
-app.register_blueprint(Forensics.forensics.forensics, url_prefix='/Forensics')
-app.register_blueprint(LFI.lfi.lfi, url_prefix='/LFI')
-app.register_blueprint(LoginPanels.loginpanels.loginpanels, url_prefix='/LoginPanels')
-app.register_blueprint(Misc.misc.misc, url_prefix='/Misc')
-app.register_blueprint(OSINT.osint.osint, url_prefix='/OSINT')
-app.register_blueprint(Stenography.stenography.stenography, url_prefix='/Stenography')
+app.register_blueprint(Crypto.crypto.crypto, url_prefix='/Challenges/Crypto')
+app.register_blueprint(Forensics.forensics.forensics, url_prefix='/Challenges/Forensics')
+app.register_blueprint(LFI.lfi.lfi, url_prefix='/Challenges/LFI')
+app.register_blueprint(LoginPanels.loginpanels.loginpanels, url_prefix='/Challenges/LoginPanels')
+app.register_blueprint(Misc.misc.misc, url_prefix='/Challenges')
+app.register_blueprint(OSINT.osint.osint, url_prefix='/Challenges/OSINT')
+app.register_blueprint(Stenography.stenography.stenography, url_prefix='/Challenges/Stenography')
 
 app.register_blueprint(adminFunc.adminFunc, url_prefix='/adminPanel')
 app.register_blueprint(info.info, url_prefix='/info')
@@ -37,7 +37,8 @@ def JWTValidate(authToken):
 def auth():
 	sessionToken = request.cookies.get('sessionToken')
 	if (not sessionToken) or (not JWTValidate(sessionToken)):
-		return redirect("/login")
+		if "Challenges" in request.path:
+			return redirect("/login")
 	return
 
 if __name__ == '__main__':
