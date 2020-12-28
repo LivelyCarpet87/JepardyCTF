8fe1d33ea3dada6eaacd7de030d03ed60d3c4247
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-12-28 21:05:52 +0800

    Challenge Bug Fixes and UI creation

--- challenges/LoginPanels/loginpanels.py
+++ challenges/LoginPanels/loginpanels.py
@@ -1,7 +1,9 @@
-from flask import jsonify, send_file, Blueprint, render_template, abort, request, g
+import flask
+from flask import jsonify, send_file, Blueprint, render_template, abort, request, g, make_response
+
 
 from . import panel0, panel1
-
+from serverBackend import data, auth
 
 import os
 template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -10,18 +12,19 @@ loginpanels = Blueprint('loginpanels', __name__,
 						template_folder=template_folder,
 						static_folder=static_folder)
 
+
 @loginpanels.route('/',methods=['GET'])
 def base():
-	return render_template("listing.html")
+	return render_template("LoginPanelsListing.html")
 
 @loginpanels.route('/panel0',methods=['GET','POST'])
-def panel0():
+def viewPanel0():
 	if flask.request.method == 'GET':
-		debugMode = request.cookies.get('Panel0DebugMode',False)
+		debugMode = request.cookies.get('Panel0DebugMode',"0")
 		response = make_response(
 			render_template("LoginPanels.html",
 				panel=0,
-				solved=data.teams["progress"].get("loginpanel0",False),
+				solved=data.getChallengeProgress(auth.JWTValidate()[0],"loginpanel0"),
 				debugMode=debugMode,
 				debug="N/A"
 			)
@@ -29,30 +32,28 @@ def panel0():
 		response.set_cookie('Panel0DebugMode', debugMode)
 		return response
 	elif flask.request.method == 'POST':
-		debugMode = request.cookies.get('Panel0DebugMode',False)
+		debugMode = request.cookies.get('Panel0DebugMode',"0")
 		user = request.form["user"]
 		pwd = request.form["pwd"]
 		success, debug, message = panel0.panel0(user,pwd)
 		if success:
-			teamDataLock.acquire()
-			data.teams["progress"]["loginpanel0"] = True
-			teamDataLock.release()
-		render_template("LoginPanels.html",
+			data.solveChallenge(auth.JWTValidate()[0],"loginpanel0")
+		return render_template("LoginPanels.html",
 			panel=0,
-			solved=data.teams["progress"].get("loginpanel0",False),
+			solved=data.getChallengeProgress(auth.JWTValidate()[0],"loginpanel0"),
 			debugMode=debugMode,
 			debug=debug,
 			message=message
 		)
 
 @loginpanels.route('/panel1',methods=['GET','POST'])
-def panel1():
+def viewPanel1():
 	if flask.request.method == 'GET':
-		debugMode = request.cookies.get('Panel1DebugMode',False)
+		debugMode = request.cookies.get('Panel1DebugMode',"0")
 		response = make_response(
 			render_template("LoginPanels.html",
 				panel=1,
-				solved=data.teams["progress"].get("loginpanel1",False),
+				solved=data.getChallengeProgress(auth.JWTValidate()[0],"loginpanel1"),
 				debugMode=debugMode,
 				debug="N/A"
 			)
@@ -60,17 +61,15 @@ def panel1():
 		response.set_cookie('Panel1DebugMode', debugMode)
 		return response
 	elif flask.request.method == 'POST':
-		debugMode = request.cookies.get('Panel1DebugMode',False)
+		debugMode = request.cookies.get('Panel1DebugMode',"0")
 		user = request.form["user"]
 		pwd = request.form["pwd"]
 		success, debug, message = panel1.panel1(user,pwd)
 		if success:
-			teamDataLock.acquire()
-			data.teams["progress"]["loginpanel1"] = True
-			teamDataLock.release()
-		render_template("LoginPanels.html",
+			data.solveChallenge(auth.JWTValidate()[0],"loginpanel1")
+		return render_template("LoginPanels.html",
 			panel=1,
-			solved=data.teams["progress"].get("loginpanel1",False),
+			solved=data.getChallengeProgress(auth.JWTValidate()[0],"loginpanel1"),
 			debugMode=debugMode,
 			debug=debug,
 			message=message
